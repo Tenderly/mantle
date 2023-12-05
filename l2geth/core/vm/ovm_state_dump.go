ef314568e26cda124a9f28214fd01c0edf30cb2f
Djordje Mitrovic <djordje@tenderly.co>
2023-12-05 14:57:13 +0100

    always use BVM

--- l2geth/core/vm/ovm_state_dump.go
+++ l2geth/core/vm/ovm_state_dump.go
@@ -1,12 +1,4 @@
 package vm
 
-import (
-	"os"
-)
-
 // UsingBVM is used to enable or disable functionality necessary for the bvm.
-var UsingBVM bool
-
-func init() {
-	UsingBVM = os.Getenv("USING_bvm") == "true"
-}
+const UsingBVM = true
